b68fdcf34e545e75eadcf9b0c52fe1e7fd5e75bc
schraderSimon <simonsch@student.matnat.uio.no>
2021-02-03 09:43:51 +0100

    made python script to run vmc iterating over alpha. Lacks Plotting

--- code/python/plot_alpha_harmonic.py
+++ code/python/plot_alpha_harmonic.py
@@ -1,5 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os, sys
-infile=np.loadtxt("../../output/sympleharmonic.csv",skiprows=1, dtype="float")
-
+import subprocess
+number_particles=10
+number_dimensions=3;
+N = int(1e6)
+omega=10
+stepLength=0.1;
+equilibration=0.1;
+alphas=np.linspace(1/8*omega,2*omega,10)
+for alpha in alphas:
+    bashCommand="./vmc %d %d %d %f %f %f %f"%(number_dimensions,number_particles,N,omega,alpha,stepLength,equilibration)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd="../cpp/build/",shell=False)
+    output, error = process.communicate()
+infile=np.loadtxt("../../output/sympleharmonic.csv",skiprows=1, dtype="float",delimiter=",",usecols=range(1,11))
