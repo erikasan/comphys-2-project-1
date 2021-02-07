9de7120d0f093ef669ce0de425564707a668094e
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-02-07 12:34:07 +0100

    .. Forgot to stage this change in previous commit lmao

--- code/cpp/WaveFunctions/wavefunction.h
+++ code/cpp/WaveFunctions/wavefunction.h
@@ -14,9 +14,9 @@ public:
     (only timing) for Metropolis)*/
     virtual double evaluate(std::vector<class Particle*> particles, int particle_id) {(void)particle_id;return evaluate(particles);};
     virtual double computeDoubleDerivative(std::vector<class Particle*> particles) = 0;
-    virtual double computeDoubleDerivativeNumerically(std::vector<class Particle*> particles) = 0;
     virtual std::vector<double> quantumForce(std::vector<class Particle*> particles, int particle_id) {(void)particle_id;return quantumForce(particles);};
     virtual std::vector<double> quantumForce(std::vector<class Particle*> particles) = 0;
+
 protected:
     int m_numberOfParameters = 0;
     std::vector<double> m_parameters = std::vector<double>();
