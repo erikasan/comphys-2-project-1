f21ef158b528eae1d0ce12d27868f666569c8748
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-03-06 12:57:44 +0100

    Cleanup

--- code/cpp/main/main.cpp
+++ code/cpp/main/main.cpp
@@ -16,35 +16,37 @@ int main(int argc, char *argv[]) {
     // Seed for the random number generator
     int seed = 2020;
 
-    int numberOfDimensions  = 3;
-    int numberOfParticles   = 1;
-    int numberOfSteps       = (int) 1e6;
-    double omega            = 25;          // Oscillator frequency.
-    double alpha            = 0.5;          // Variational parameter.
-    double stepLength       = 0.1;          // Metropolis step length.
-    double equilibration    = 0.1;          // Amount of the total steps used
+    int numberOfDimensions = 3;
+    int numberOfParticles  = 1;
+    int numberOfSteps      = (int) 1e6;
+    double omega           = 25;           // Oscillator frequency.
+    double alpha           = 0.5;          // Variational parameter.
+    double stepLength      = 0.1;          // Metropolis step length.
+    double equilibration   = 0.1;          // Amount of the total steps used
+
     if (argc>1){
-    try{
-        numberOfDimensions=atoi(argv[1]);
-        numberOfParticles   = atoi(argv[2]);
-        numberOfSteps   = atoi(argv[3]);
-        omega= atoi(argv[4]);
-        alpha=atof(argv[5]);
-        stepLength=atof(argv[6]);
-        equilibration=atof(argv[7]);
+      try{
+          numberOfDimensions = atoi(argv[1]);
+          numberOfParticles  = atoi(argv[2]);
+          numberOfSteps      = atoi(argv[3]);
+          omega              = atoi(argv[4]);
+          alpha              = atof(argv[5]);
+          stepLength         = atof(argv[6]);
+          equilibration      = atof(argv[7]);
+      }
+      catch (int e){
+        cout << "An exception occurred. Exception Nr. " << e << '\n';
+      }
     }
-    catch (int e)
-    {
-      cout << "An exception occurred. Exception Nr. " << e << '\n';
-    }
-    }
+
     System* system = new System(seed);
     system->setOmega(omega);
-    system->setHamiltonian              (new HarmonicOscillator(system, omega));
-    system->setWaveFunction             (new SimpleGaussian(system, alpha));
-    system->setInitialState             (new RandomUniform(system, numberOfDimensions, numberOfParticles));
-    system->setEquilibrationFraction    (equilibration);
-    system->setStepLength               (stepLength);
-    system->runMetropolisSteps          (numberOfSteps,true);
+    system->setHamiltonian           (new HarmonicOscillator(system, omega));
+    system->setWaveFunction          (new SimpleGaussian(system, alpha));
+    system->setInitialState          (new RandomUniform(system, numberOfDimensions, numberOfParticles));
+    system->setEquilibrationFraction (equilibration);
+    system->setStepLength            (stepLength);
+    system->runMetropolisSteps       (numberOfSteps,true);
+
     return 0;
 }
