03d18b35cfef04ba363f235c9c9f18e97c4b14c3
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-03-07 16:45:23 +0100

    Removed unnecessary function call

--- code/cpp/GDsampler.cpp
+++ code/cpp/GDsampler.cpp
@@ -4,7 +4,6 @@
 #include "WaveFunctions/wavefunction.h"
 
 void GDsampler::gdsampler(std::vector<class Particle*> particles, double localEnergy){
-  particles = m_system->getParticles();
   m_system->getWaveFunction()->sample(particles, localEnergy);
   return;
 }
